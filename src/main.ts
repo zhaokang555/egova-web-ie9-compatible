28ca123d353edbb810d44c8337d98dfafd120fa9
Zhao Kang <1003994970@qq.com>
2018-07-23 17:09:47 +0800

    引入iview支持, 注释掉bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './stores/index';
-import '@/assets/bootstrap/css/bootstrap.min.css';
-import '@/assets/bootstrap/css/bootstrap-theme.min.css';
+import iView from "iview";
+import "iview/dist/styles/iview.css";
+// import '@/assets/bootstrap/css/bootstrap.min.css';
+// import '@/assets/bootstrap/css/bootstrap-theme.min.css';
 
 Vue.config.productionTip = false;
+Vue.use(iView);
 
 new Vue({
     router,
--- src/views/syste-mmanage/index.vue
+++ src/views/syste-mmanage/index.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="system-manage">
         {{msg}}
+        <Button>Default</Button>
+        <Button type="primary">Primary</Button>
+        <Button type="ghost">Ghost</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="text">Text</Button>
+        <br><br>
+        <Button type="info">Info</Button>
+        <Button type="success">Success</Button>
+        <Button type="warning">Warning</Button>
+        <Button type="error">Error</Button>
     </div>
 </template>
 
