4bfac9eda4f58ef7ee5ba8ecac2aee015ab1185a
songjh <799724471@qq.com>
2018-07-24 09:05:32 +0800

    [M]系统管理页面

--- src/views/syste-mmanage/index.vue
+++ src/views/syste-mmanage/index.vue
@@ -11,6 +11,7 @@
         <Button type="success">Success</Button>
         <Button type="warning">Warning</Button>
         <Button type="error">Error</Button>
+         <Table :columns="columns" :data="data"></Table>
     </div>
 </template>
 
@@ -20,5 +21,99 @@
     @Component
     export default class SystemManage extends Vue {
         public msg = "系统管理";
+        public columns = [
+            {
+                title: 'Name',
+                key: 'name',
+                render: (h: any, params: any) => {
+                    return h('div', [
+                        h('Icon', {
+                            props: {
+                                type: 'person'
+                            }
+                        }),
+                        h('strong', params.row.name)
+                    ]);
+                }
+            },
+            {
+                title: 'Age',
+                key: 'age'
+            },
+            {
+                title: 'Address',
+                key: 'address'
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                width: 150,
+                align: 'center',
+                render: (h: any, params: any) => {
+                    return h('div', [
+                        h('Button', {
+                            props: {
+                                type: 'primary',
+                                size: 'small'
+                            },
+                            style: {
+                                marginRight: '5px'
+                            },
+                            on: {
+                                click: () => {
+                                    this.show(params.index)
+                                }
+                            }
+                        }, 'View'),
+                        h('Button', {
+                            props: {
+                                type: 'error',
+                                size: 'small'
+                            },
+                            on: {
+                                click: () => {
+                                    this.remove(params.index)
+                                }
+                            }
+                        }, 'Delete')
+                    ]);
+                }
+            }
+        ];
+        public data = [
+            {
+                name: 'John Brown',
+                age: 18,
+                address: 'New York No. 1 Lake Park',
+                date: '2016-10-03'
+            },
+            {
+                name: 'Jim Green',
+                age: 24,
+                address: 'London No. 1 Lake Park',
+                date: '2016-10-01'
+            },
+            {
+                name: 'Joe Black',
+                age: 30,
+                address: 'Sydney No. 1 Lake Park',
+                date: '2016-10-02'
+            },
+            {
+                name: 'Jon Snow',
+                age: 26,
+                address: 'Ottawa No. 2 Lake Park',
+                date: '2016-10-04'
+            }
+        ];
+        // public show(index) {
+        //         this.$modal.info({
+        //             title: 'User Info',
+        //             content: `Name：${this.data6[index].name}<br>Age：${this.data6[index].age}<br>Address：${this.data6[index].address}`
+        //         })
+        // }
+        // public remove (index) {
+        //     this.data.splice(index, 1);
+        // }
     }
 </script>
